a3c597ce5f2a2c8353c5d068030aa0c583622df4
Ulises <xnuch5@gmail.com>
2024-04-10 09:22:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res){
+    res.send('Web inicial de mi API');
+});
+
+app.get('/saludo', function (req, res){
+    res.send('Hola mundo');
+});
+
+app.get('/despedido', function (req, res){
+    res.send('Adios mundo cruel');
+});
+app.listen(3000, function(){
+    console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
+});
